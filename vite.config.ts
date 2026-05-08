e00517206e73b0fa12466170d163877f4d38328c
Nova-Shen1 <shenzhenxuan817@gmail.com>
2026-05-08 04:44:40 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/cooking/',  // 👈 加上这一行，cooking 是你的仓库名
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
